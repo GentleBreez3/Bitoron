ec17c12b6a3c8bbde3542223c8905c494817056c
sCar3Cr0w25 <wretchedegg8@gmail.com>
2020-12-10 21:58:09 +0600

    decorators & access permission

--- crud/accounts/urls.py
+++ crud/accounts/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
+    path('user/', views.userPage, name="user-page"),
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
-    path('logout/', views.logoutPage, name="logout"),
+    path('logout/', views.logoutUser, name="logout"),
     path('products/',views.products, name="products"),
     path('customer/<str:pk>/', views.customer, name="customer"),
     path('create_order/<str:pk>/', views.createOrder, name="create_order"),
